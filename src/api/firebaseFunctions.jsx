0095c80aa2f61aa99f685490b69fdc384110ad75
Shaon Bhattacharjee <bshaon19@gmail.com>
2023-12-25 12:06:38 +0530

    user added in db

--- src/api/firebaseFunctions.jsx
+++ src/api/firebaseFunctions.jsx
@@ -5,26 +5,36 @@ import { database } from "./firebase";
 import {set,ref } from "firebase/database";
 
 
-
-
-
-
-
 const  SignupFunction=async(callback) => {
     try{
 const result=await signInWithPopup(auth,googleAuthProvider);
 localStorage.setItem('token',result.user.accessToken);
 localStorage.setItem("user",JSON.stringify(result.user));
-console.log(result);
+
+ let username=result.user.displayName
+ let email=result.user.email
+
+ let userData={
+    name: username,email:email,score:null
+ }
+/////creating user in database
+ registerUser(username,userData)
+
 callback()
 }catch(err){
   console.log(err)
+
 }}
 
 
-// const registerUser=(key,data) => {
-//     set(ref(database,"user/"+ key), data);
-//    }
+const registerUser=(key,data) => {
+    set(ref(database,"user/"+ key), data);
+   }
+
+
+
+
+
 
 
 
